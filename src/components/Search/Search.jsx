e7e786f54d37bb1b3f1009a63f9bc1c3097bfa86
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-04-08 17:49:14 -0700

    feat: fetching the recipes and saving it in state

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Search.module.css'
 
-const Search = () => {
+const Search = ({ getData }) => {
+
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputValue = (event) => {
+    const { value } = event.target
+    setInputValue(value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getData(inputValue)
+
+  }
+
   return (
-    <form className={style.search}>
-        <input name='search' placeholder='Search Recipes' id='search'/>
+    <form onSubmit={handleSubmit} className={style.search}>
+        <input 
+          name='search' 
+          onChange={handleInputValue} 
+          value={inputValue} 
+          placeholder='Search Recipes' 
+          id='search'
+        />
         <button type='submit'>Search</button>
     </form>
   )
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react"
 import Search from "../../components/Search/Search"
 import style from './Homepage.module.css'
 
+
 const Homepage = () => {
+
+    const [loading, setLoading] = useState(false)
+    const [recipes, setRecipes] = useState([])
+
+
+    const getDataFromSearch = (getData) => {
+
+
+        setLoading(true);
+
+        async function getRecipes(){
+            const apiResponse = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=0b486e71c68f4b9c9a0e5430c2d4b447&query=${getData}`)
+            const result = await apiResponse.json()
+
+            const { results } = result
+            if(results && results.length > 0 ){
+                setLoading(false)
+                setRecipes(results)
+            }
+
+        }
+
+        getRecipes()
+    }
+
+    console.log(loading, recipes, "The data is here");
+
     return (
-        <div className="homepage">
-            <Search/>
+        <div className={style.homepage}>
+            <Search getData={getDataFromSearch} />
         </div>
     )
 }
